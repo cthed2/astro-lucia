7d8095fae86d4eeace3e33f2eea2d586b2c2e199
cthed2 <cristhian.ipanaque@gmail.com>
2024-04-15 00:25:58 -0500

    agregue documentacion para hacer pruebas con google oauth2

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,17 @@
 import { defineConfig } from 'astro/config';
+import cloudflare from "@astrojs/cloudflare";
+
+import db from "@astrojs/db";
 
 // https://astro.build/config
 export default defineConfig({
-    // ...
-	vite: {
-		optimizeDeps: {
-			exclude: ["oslo"]
-		}
-	}
+  // ...
+  vite: {
+    optimizeDeps: {
+      exclude: ["oslo"]
+    }
+  },
+  output: "server",
+  adapter: cloudflare(),
+  integrations: [db()]
 });
